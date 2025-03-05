fd51138b8e7b8391ae87575f81878573a0952145
Dipakkhaire23 <dipakkhaire23@gmail.com>
2025-03-05 11:34:02 +0530

    nursery firstcomiit

--- backend/assistant/src/main/java/com/Legal_assistant/assistant/Controller/CustomerController.java
+++ backend/assistant/src/main/java/com/Legal_assistant/assistant/Controller/CustomerController.java
@@ -2,7 +2,9 @@ package com.Legal_assistant.assistant.Controller;
 
 import com.Legal_assistant.assistant.Model.Customer;
 import com.Legal_assistant.assistant.Services.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +19,14 @@ public class CustomerController {
 
     // Save customer
     @PostMapping("/save")
-    public Customer saveCustomer(@RequestBody Customer customer) {
+    public Customer saveCustomer(@Valid @RequestBody Customer customer, BindingResult bindingResult) {
+        // If there are validation errors, return a bad request response
+        if (bindingResult.hasErrors()) {
+            // You can return error messages to the client based on validation failures
+            StringBuilder errorMessage = new StringBuilder();
+            bindingResult.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append("\n"));
+            throw new IllegalArgumentException(errorMessage.toString());
+        }
         return customerService.saveCustomer(customer);
     }
 
--- backend/assistant/src/main/java/com/Legal_assistant/assistant/Model/Customer.java
+++ backend/assistant/src/main/java/com/Legal_assistant/assistant/Model/Customer.java
@@ -2,9 +2,13 @@ package com.Legal_assistant.assistant.Model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,10 +22,16 @@ public class Customer {
     @Column(name = "ID")
     private Long id;
     @Column(name = "Name")
+    @NotNull(message = "not null")
     private String name;
+    @NotNull(message = "Mobile number cannot be null")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number must be 10 digits")
     private String mobileNumber;
+    @NotNull(message = "Address cannot be null")
+    @Size(min = 5, max = 255, message = "Address must be between 5 and 255 characters")
     private String address;
     private String village;
+    @NotNull(message = "Quantity of plants purchased cannot be null")
     private int quantityOfPlantsPurchased;
 
 }
